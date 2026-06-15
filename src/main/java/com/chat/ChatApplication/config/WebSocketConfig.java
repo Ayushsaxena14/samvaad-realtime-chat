a8c43b774245bbff3297a1a04eb1a75f2d79f125
Ayush Saxena <ayushsaxena1405@gmail.com>
2026-06-15 13:14:06 +0530

    Update WebSocketConfig to use setAllowedOriginPatterns

--- src/main/java/com/chat/ChatApplication/config/WebSocketConfig.java
+++ src/main/java/com/chat/ChatApplication/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
     public void registerStompEndpoints(StompEndpointRegistry registry)
     {
        registry.addEndpoint("/chat")
-               .setAllowedOrigins("*")
+               .setAllowedOriginPatterns("*")
                .withSockJS();
     }
 //    @Override
